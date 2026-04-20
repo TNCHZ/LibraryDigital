66910cdd6834d12b36594b2f4518b1613301ce13
tu2004latoi <tu2004latoi@gmail.com>
2026-04-20 16:08:25 +0700

    TV-39 Phát triển chức năng quản lý sách

--- library_digital/models/category_book.py
+++ library_digital/models/category_book.py
@@ -1,8 +1,8 @@
 from library_digital.extensions import db
-from .base import BaseModel
 
-class CategoryBook(BaseModel):
+class CategoryBook(db.Model):
     __tablename__ = "category_book"
 
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable=False)
-    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id", ondelete="CASCADE"), nullable=False)
